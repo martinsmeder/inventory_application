be4913a3d3527a395173121c3fd805909a64f2c6
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-03-12 20:10:36 +0100

    Added list and detail views and controllers

--- controllers/consoleController.js
+++ controllers/consoleController.js
@@ -1,14 +1,40 @@
 const Console = require("../models/console");
+const Game = require("../models/game");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Consoles.
 exports.console_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Console list");
+  // Get all consoles from database and sort in ascending order
+  const allConsoles = await Console.find().sort({ name: 1 }).exec();
+
+  // Render using console_list.pug view
+  res.render("console_list", {
+    title: "Console List",
+    console_list: allConsoles,
+  });
 });
 
 // Display detail page for a specific Console.
 exports.console_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Console detail: ${req.params.id}`);
+  // Get details about console and all their games from database (in parallel)
+  const [consoleModel, allGamesOnConsole] = await Promise.all([
+    Console.findById(req.params.id).exec(),
+    Game.find({ console_model: req.params.id }, "name description").exec(),
+  ]);
+
+  if (consoleModel === null) {
+    // No results
+    const err = new Error("Console not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  // Render using console_detail.pug view
+  res.render("console_detail", {
+    title: "Console Detail",
+    console_model: consoleModel,
+    console_games: allGamesOnConsole,
+  });
 });
 
 // Display Console create form on GET.
--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,18 +1,51 @@
 const Game = require("../models/game");
+const Console = require("../models/console");
 const asyncHandler = require("express-async-handler");
 
+// Display home page.
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  // Get games and consoles from database (in parallel)
+  const [numGames, numConsoles] = await Promise.all([
+    Game.countDocuments({}).exec(),
+    Console.countDocuments({}).exec(),
+  ]);
+
+  // Render using index.pug view
+  res.render("index", {
+    title: "Game Inventory Home",
+    game_count: numGames,
+    console_count: numConsoles,
+  });
 });
 
 // Display list of all games.
 exports.game_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Game list");
+  // Get all games and sort them in ascending order
+  const allGames = await Game.find({}, "name console_model")
+    .sort({ name: 1 })
+    .populate("console_model") // Reference document from console_model collection
+    .exec();
+
+  // Render using game_list.pug view
+  res.render("game_list", { title: "Game List", game_list: allGames });
 });
 
 // Display detail page for a specific game.
 exports.game_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Game detail: ${req.params.id}`);
+  // Get game details from database
+  const game = await Game.findById(req.params.id)
+    .populate("console_model") // Reference document from console_model collection
+    .exec();
+
+  if (game === null) {
+    // No results
+    const err = new Error("Game not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  // Render using game_detail.pug view
+  res.render("game_detail", { title: game.name, game: game });
 });
 
 // Display game create form on GET.
